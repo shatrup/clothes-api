0431e9992ad465368b03dd90d1cf7dfbdd1aaf76
Shatru <42533683+shatrup@users.noreply.github.com>
2021-06-18 23:12:35 +0530

    Update password

--- config/key_path.js
+++ config/key_path.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // mongoURI: "mongodb+srv://node_830:node830@firtstime-43hcc.mongodb.net/nodedb?retryWrites=true&w=majority",
-  mongoURI: "mongodb+srv://shatru:shatru830@cluster0.m9b8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  // mongoURI: "mongodb+srv://xyz:password@firtstime-43hcc.mongodb.net/nodedb?retryWrites=true&w=majority",
+  mongoURI: "Enter your mongo URL",
   secretOrKey: "secretkey"
 };
 
